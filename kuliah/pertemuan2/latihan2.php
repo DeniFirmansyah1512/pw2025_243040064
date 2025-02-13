d645825564b329126db7fcb8eef3564cc3a2d84a
DeniFirmansyah1512 <denifirmansyah1512>
2025-02-13 18:36:54 +0700

    tugas_1

---
+++ kuliah/pertemuan2/latihan2.php
@@ -0,0 +1,9 @@
+<?php
+$nama = "DeniFirmansyah" ;
+$email = "denifirmanysah1512@gmail.com";
+$pekerjaan = "Mahasiswa";
+$universitas = "Universitas pasundan";
+echo "<h4>$nama</h4>";
+echo "<h4>$email</h4>";
+echo "<h4>halo saya $nama saya kuliah di $universitas email saya $email </h4>";
+?>
