212217cf4491e084f7dd44f3491311a51f86557a
DeniFirmansyah1512 <denifirmansyah1512>
2025-03-02 15:21:40 +0700

    tugas 3

--- kuliah/pertemuan2/latihan1.php
+++ kuliah/pertemuan2/latihan1.php
@@ -1,12 +1,15 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>latihan php</title>
 </head>
+
 <body>
-   <h1><?php echo "HELLO WORLD!"; ?></h1>
-   <p><?php echo "sdbhfwhgfehfgwhfw"; ?></p>
+    <h1><?php echo "HELLO WORLD!"; ?></h1>
+    <p><?php echo "sdbhfwhgfehfgwhfw"; ?></p>
 </body>
+
 </html>
---
+++ kuliah/pertemuan4/latihan1.php
@@ -0,0 +1,14 @@
+<?php
+
+// while //
+$i =10;
+while ($i >= 1 ) {
+
+    echo "hello world $i <br>";
+    $i = $i -1 ;
+} 
+
+
+for($i = 1; $i <= 10; $i++) {
+    echo"hello world ";
+}
