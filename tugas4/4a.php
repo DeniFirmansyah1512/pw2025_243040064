00f36e850fd02e653e597a38c50b2090a48cd392
DeniFirmansyah1512 <denifirmansyah1512>
2025-03-15 20:51:56 +0700

    tugas4

---
+++ tugas4/4a.php
@@ -0,0 +1,8 @@
+<?php
+// Membuat array dengan elemen yang diberikan
+$kata = ["epan", "cipta", "akan", "u", "asa", "esok"];
+
+// Menampilkan kalimat dengan menggabungkan elemen array
+echo "Masa depanmu d" . $kata[0] . " oleh apa yang kau " . $kata[1] . 
+     " sekarang, " . $kata[2] . " bukan " . $kata[5] . ".";
+?>
